18d5913983519afc981d4cddf9fd1bcbcf1fa51d
Nikhil Jangid <117031542+njangid22@users.noreply.github.com>
2024-12-11 23:25:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
 
-# Ensure that static files are collected into the correct directory
-python manage.py collectstatic --noinput
 
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
